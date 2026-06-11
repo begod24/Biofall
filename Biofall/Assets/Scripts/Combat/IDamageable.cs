d1e1405f98adcab975b5d711e66f7032b60cbca9
Bekbolat <bekbolat.aldiyarov@gmail.com>
2026-06-11 20:40:28 +0200

    project clean

--- Biofall/Assets/Scripts/Combat/IDamageable.cs
+++
@@ -1,9 +0,0 @@
-namespace Biofall.Combat
-{
-    /// <summary>Anything that can take damage. Implemented by player and enemies via shared components.</summary>
-    public interface IDamageable
-    {
-        bool IsAlive { get; }
-        void ApplyDamage(in DamageInfo damageInfo);
-    }
-}
